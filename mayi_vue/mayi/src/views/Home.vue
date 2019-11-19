f861d473cb251d70e5cfe577c0fb0c7d9db0eeb0
“t“fanbaohua <2019828026@qq.com>
2019-11-19 15:05:32 +0800

    配置了console.log

--- mayi_vue/mayi/src/views/Home.vue
+++ mayi_vue/mayi/src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <van-tabs v-model="active" sticky>
+      <van-tab
+        v-for="index in 8"
+        :key="index"
+        :title="'标签 ' + index"
+        @change="current()"
+      >
+        内容 {{ index }}
+      </van-tab>
+      <div class="sousuo">
+        <img src="../../public/images/home/sousuo.png" />
+      </div>
+    </van-tabs>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      active: ""
+    };
+  },
+  methods: {
+    current() {
+      // console.log(active);
+    }
   }
 };
 </script>
+
+<style lang="less" scoped>
+.van-tabs {
+  padding-right: 14%;
+}
+.sousuo {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  width: 12%;
+  box-shadow: -10px 0px 10px #fff;
+  // height: 5%;
+  // background-color: #00f;
+  padding-top: 2%;
+}
+</style>
